4c56c43047f8dcbd5df58a237848d2ae9a9c86f3
Ashish Kunar <ashkumar1203@gmail.com>
2026-05-02 16:51:38 +0530

    third commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <section
-      className="h-[340px] md:h-[410px] bg-cover md:bg-cover bg-contain bg-no-repeat bg-center -mt-13 -mb-11 md:mt-0 md:mb-0"
+      className="h-[340px] md:h-[410px] bg-cover md:bg-cover bg-contain bg-no-repeat bg-center -mt-13 -mb-16 md:mt-0 md:mb-0"
       style={{
         backgroundImage: "url('/images/BannerImg.png')",
       }}
